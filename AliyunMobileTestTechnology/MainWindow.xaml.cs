9d9e84a40ca367c4bc7d2b239e752fcf46439394
WangMoYing <moying.wmy@gmail.com>
2013-08-12 14:54:03 +0800

    add

---
+++ AliyunMobileTestTechnology/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace AliyunMobileTestTechnology
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑#FF307195
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //System.Threading.Thread.Sleep(5000);
+            //SolidColorBrush sb = this.test.Resources["BlackBrush"] as SolidColorBrush;
+        }
+
+        //public Color BrushToColor(Brush bush) 
+        //{
+        //    return (Color)ColorConverter.ConvertFromString(bush.ToString()); 
+        //}
+    }
+}
